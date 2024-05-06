94d3bcb7d6cddbeae6e4ea4e2037d353e9dd1eeb
alvindo <alvindo56@gmail.com>
2024-05-06 21:22:13 +0700

    feat: completing home screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 import ThemeContextProvider from "./App/context/ThemeContext";
 import AppNavigator from "./App/navigation/AppNavigator";
 import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "react-native";
 
 export default function App() {
     useEffect(() => {
--- App/Screen/Home/HomeScreen.js
+++ App/Screen/Home/HomeScreen.js
@@ -1,11 +1,25 @@
 import { Ionicons } from "@expo/vector-icons";
-import React from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import { Alert, FlatList, Image, Text, TextInput, View } from "react-native";
+import {
+    Alert,
+    Dimensions,
+    FlatList,
+    Image,
+    ImageBackground,
+    Keyboard,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+} from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import UserAvatar from "react-native-user-avatar";
 import { useTheme } from "../../context/ThemeContext";
 import { borders } from "../../shared/constant/borders";
 import { typography } from "../../shared/constant/typography";
+import Carousel from "react-native-snap-carousel";
 
 const EQUIPMENT = [
     {
@@ -72,119 +86,159 @@ const EQUIPMENT = [
 
 const HomeScreen = () => {
     const { theme } = useTheme();
-    const [refreshing, setRefreshing] = React.useState(false);
+    const isCarousel = useRef(null);
     const { control, handleSubmit } = useForm({
         mode: "onSubmit",
         defaultValues: {
             search: "",
         },
     });
-
-    const onRefresh = React.useCallback(() => {
-        setRefreshing(true);
-        setTimeout(() => {
-            setRefreshing(false);
-        }, 2000);
-    }, []);
+    const styles = useMemo(
+        () =>
+            StyleSheet.create({
+                option: {
+                    backgroundColor: theme.colors.background,
+                    borderRadius: borders.radiusLarge,
+                    opacity: 0.5,
+                    padding: 20,
+                    width: 100,
+                },
+                optionText: {
+                    position: "absolute",
+                    color: theme.colors.text,
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                optionLayer: {
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderColor: theme.colors.text,
+                    borderWidth: 1,
+                    borderRadius: borders.radiusLarge,
+                },
+            }),
+        []
+    );
 
     const onSubmit = (data) => {
         Alert.alert("Search", "Searching..." + data.search);
     };
 
     return (
-        <>
-            <Image
-                style={{ position: "absolute", zIndex: 0, objectFit: "cover", width: "100%", height: "100%" }}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <ImageBackground
+                style={{ width: "100%", height: Dimensions.get("window").height }}
+                resizeMode="cover"
                 source={require("../../../assets/home.jpg")}
-            />
-            <View style={[{ zIndex: 1, position: "relative", width: '100%' }, theme.padding]}>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        alignItems: "center",
-                        paddingVertical: 20,
-                    }}
-                >
-                    <Text style={[typography.header, { color: theme.colors.text }]}>Eazy Camp</Text>
-                    <UserAvatar size={35} name="Alvindo" />
-                </View>
-                <Text style={[typography.title, {color: theme.colors.text, marginBottom: 20}]}>Welcome Back, Alvindo</Text>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        alignItems: "center",
-                        borderRadius: borders.radiusLarge,
-                        width: "100%",
-                        backgroundColor: theme.colors.background,
-                    }}
-                >
-                    <Ionicons
-                        style={{ marginHorizontal: 10 }}
-                        name="search-outline"
-                        size={24}
-                        color={theme.colors.text}
-                    />
-                    <Controller
-                        control={control}
-                        name="search"
-                        render={({ field: { onChange, onBlur, value } }) => (
-                            <TextInput
-                                style={{ flex: 1, padding: 10, color: theme.colors.text }}
-                                onChangeText={onChange}
-                                returnKeyType="search"
-                                onBlur={onBlur}
-                                value={value}
-                                placeholder="Cari peralatan camping..."
-                                placeholderTextColor={theme.colors.text}
-                                onSubmitEditing={handleSubmit(onSubmit)}
-                            />
-                        )}
-                    />
-                </View>
-                <Text style={[typography.title, {color: theme.colors.text, marginVertical: 50}]}>
-                    The journey of thousand miles begins with a single step. ~ 老子 (Lao Tzu).
-                </Text>
-                <View>
-                    
-                </View>
-                <FlatList
-                    data={EQUIPMENT}
-                    horizontal
-                    keyExtractor={(item) => item.id}
-                    refreshing={refreshing}
-                    onRefresh={onRefresh}
-                    contentContainerStyle={{ gap: 10 }}
-                    renderItem={({ item }) => (
-                        <View
-                            style={[
-                                {
-                                    flex: 1,
-                                    flexDirection: "column",
-                                    backgroundColor: theme.colors.primary,
-                                    borderRadius: borders.radiusSmall,
-                                    paddingBottom: 10,
-                                },
-                                theme.shadow,
-                            ]}
-                        >
-                            <Image
-                                source={{ uri: item.image }}
-                                style={{
-                                    width: "100%",
-                                    height: 300,
-                                    borderRadius: borders.radiusSmall,
-                                    objectFit: "cover",
-                                    marginBottom: 10,
-                                }}
-                            />
-                            <Text style={{ color: theme.colors.text, paddingHorizontal: 10 }}>{item.name}</Text>
-                            <Text style={{ color: theme.colors.text, paddingHorizontal: 10 }}>{item.description}</Text>
+            >
+                <View style={[{ flex: 1 }, theme.padding]}>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            paddingVertical: 20,
+                        }}
+                    >
+                        <Text style={[typography.header, { color: theme.colors.text }]}>Eazy Camp</Text>
+                        <UserAvatar size={35} name="Alvindo" />
+                    </View>
+                    <Text style={[typography.title, { color: theme.colors.text, marginBottom: 20 }]}>
+                        Selamat Datang, Alvindo
+                    </Text>
+                    <View
+                        style={{
+                            flexDirection: "row",
+                            alignItems: "center",
+                            borderRadius: borders.radiusLarge,
+                            width: "100%",
+                            backgroundColor: theme.colors.background,
+                        }}
+                    >
+                        <Ionicons
+                            style={{ marginHorizontal: 10 }}
+                            name="search-outline"
+                            size={24}
+                            color={theme.colors.text}
+                        />
+                        <Controller
+                            control={control}
+                            name="search"
+                            render={({ field: { onChange, onBlur, value } }) => (
+                                <TextInput
+                                    style={{ flex: 1, padding: 10, color: theme.colors.text }}
+                                    onChangeText={onChange}
+                                    returnKeyType="search"
+                                    onBlur={onBlur}
+                                    value={value}
+                                    placeholder="Cari peralatan camping..."
+                                    placeholderTextColor={theme.colors.text}
+                                    onSubmitEditing={handleSubmit(onSubmit)}
+                                />
+                            )}
+                        />
+                    </View>
+                    <Text style={[typography.title, { color: theme.colors.text, marginVertical: 50 }]}>
+                        The journey of thousand miles begins with a single step. ~ 老子 (Lao Tzu).
+                    </Text>
+                    <View style={{ flexDirection: "row", justifyContent: "space-around", marginBottom: 50 }}>
+                        <View style={styles.optionLayer}>
+                            <TouchableOpacity style={styles.option} />
+                            <Text style={styles.optionText}>Lokasi</Text>
                         </View>
-                    )}
-                />
-            </View>
-        </>
+                        <View style={[styles.optionLayer, { backgroundColor: theme.colors.primary }]}>
+                            <TouchableOpacity style={styles.option} />
+                            <Text style={styles.optionText}>Peralatan</Text>
+                        </View>
+                        <View style={styles.optionLayer}>
+                            <TouchableOpacity style={styles.option} />
+                            <Text style={styles.optionText}>Pemandu</Text>
+                        </View>
+                    </View>
+                    <View style={{ height: 400 }}>
+                        <Carousel
+                            ref={isCarousel}
+                            data={EQUIPMENT}
+                            sliderWidth={Dimensions.get("window").width - 40}
+                            itemWidth={Dimensions.get("window").width - 100}
+                            onSnapToItem={(index) => console.log(index)}
+                            loop
+                            renderItem={({ item }) => (
+                                <View
+                                    style={[
+                                        {
+                                            flex: 1,
+                                            height: 400,
+                                            flexDirection: "column",
+                                            backgroundColor: theme.colors.primary,
+                                            borderRadius: borders.radiusLarge,
+                                            paddingBottom: 10,
+                                        },
+                                        theme.shadow,
+                                    ]}
+                                >
+                                    <Image
+                                        source={{ uri: item.image }}
+                                        style={{
+                                            width: "100%",
+                                            height: 300,
+                                            borderTopLeftRadius: borders.radiusLarge,
+                                            borderTopRightRadius: borders.radiusLarge,
+                                            objectFit: "cover",
+                                            marginBottom: 10,
+                                        }}
+                                    />
+                                    <Text style={{ color: theme.colors.text, paddingHorizontal: 10 }}>{item.name}</Text>
+                                    <Text style={{ color: theme.colors.text, paddingHorizontal: 10 }}>
+                                        {item.description}
+                                    </Text>
+                                </View>
+                            )}
+                        />
+                    </View>
+                </View>
+            </ImageBackground>
+        </TouchableWithoutFeedback>
     );
 };
 
