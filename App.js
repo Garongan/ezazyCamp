1263318d8cc7b7831bf5a95c1405cef6184ba18a
Garongan <alvindo56@gmail.com>
2024-07-22 12:44:42 +0700

    feat: move to sdk 51

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
+import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ThemeContextProvider from "./App/context/ThemeContext";
 import AppNavigator from "./App/navigation/AppNavigator";
-import * as SplashScreen from "expo-splash-screen";
-import { StatusBar } from "react-native";
 
 export default function App() {
-    useEffect(() => {
-        SplashScreen.hideAsync();
-    }, []);
+	useEffect(() => {
+		SplashScreen.hideAsync();
+	}, []);
 
-    return (
-        <ThemeContextProvider>
-            <SafeAreaProvider>
-                <AppNavigator />
-            </SafeAreaProvider>
-        </ThemeContextProvider>
-    );
+	return (
+		<ThemeContextProvider>
+			<SafeAreaProvider>
+				<AppNavigator />
+			</SafeAreaProvider>
+		</ThemeContextProvider>
+	);
 }
